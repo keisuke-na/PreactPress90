fce6e36845360b9cdd8a9e955f52429e9a5b9ebe
Keisuke Nagamori <keisukenagamori@gmail.com>
2025-09-23 12:15:29 +0900

    Add GitHub Actions workflow and configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,14 @@ import { staticBlogPlugin } from './src/plugins/static-blog-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	// GitHub Pages: https://username.github.io/PreactPress90/
+	base: '/PreactPress90/',
 	plugins: [
 		preact(),
 		staticBlogPlugin({
 			postsDir: 'posts',
 			outputDir: 'dist',
-			baseUrl: '/'
+			baseUrl: '/PreactPress90/'
 		})
 	],
 });
